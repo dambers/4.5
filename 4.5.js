825d5a36f2f55770b0f433ad1ad3e8240dc63d51
dambers <97996456+dambers@users.noreply.github.com>
2025-02-14 22:22:41 -0600

    Add files via upload

---
+++ 4.5.js
@@ -0,0 +1,41 @@
+// string method
+console.log("Good evening my name is " +  "Demetris");
+
+// if conditional statement
+const age = 50;
+
+
+if(age < 30) {
+    greeting = "have a good night";
+} else {
+    greeting = "The door is that way!"
+}
+
+//switch statement
+let yoga;
+switch (new yoga().getyoga()) {
+
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+
+}
+
+// number method
+const PI = 3.141596;
+
+const num = PI.toFixed(3);
+
+console.log(num);
